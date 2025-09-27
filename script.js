41c4f174c8bc2b861df4f599fd4bb3818b176284
StigMikalMJ-KP <johansenme@afk.no>
2025-09-27 12:24:33 +0200

    card draw update

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ let guess;
 let mousePosX;
 let mousePosY;
 
+let deckLength = 52;
 let cardChars = ["H", "K", "R", "S"];
-let selectedCard = {};
-let usedCards = [];
+let selectedCard = {element: null, image: null, cardNum: null};
+let drawnCards = [];
 let cardOnTop = randomCard();
 
 
@@ -23,33 +24,28 @@ const starterCardCount = 5;
 
 
 // ====== Functions ======
-function makePlayerCards(handCardsE){    
-    for(let i = 0; i < starterCardCount; i++){
-        let randCard = randomCard();
-        let newCard = document.createElement("div");
+function giveDrawnCard(card, object){
+    let newCard = document.createElement("div");
 
-        newCard.innerHTML = `<img src="Assets/Kortbilder/${randCard}.png" alt=""></img>`;
-        newCard.className = "player-hand-cards";
-
-        newCard.addEventListener("click", function(){
-            if(selectedCard.element && selectedCard.element !== newCard){
-                selectedCard.element.firstChild.id = "";
+    newCard.innerHTML = `<img src="Assets/Kortbilder/${card}.png" alt="">`;
+    newCard.className = "player-hand-cards";
+    newCard.addEventListener("click", function(){
+        if(selectedCard.element && selectedCard.element !== newCard){
+            selectedCard.element.firstChild.id = "";
+        }
+        if(selectedCard.element !== newCard){
+            selectedCard = {
+                element: newCard,   
+                image: `<img src="Assets/Kortbilder/${card}.png" alt="">`,
+                cardNum: card
             }
-            if(selectedCard.element !== newCard){
-                selectedCard = {
-                    element: newCard,   
-                    image: `<img src="Assets/Kortbilder/${randCard}.png" alt=""></img>`,
-                    cardNum: randCard
-                }
-                selectedCard.element.firstChild.id = "selectedCard";
-            } else{
-                selectedCard.element.firstChild.id = "";
-                selectedCard.element = null;
-            }  
-        });
-
-        handCardsE.appendChild(newCard);    
-    }
+            selectedCard.element.querySelector("img").id = "selectedCard";
+        } else{
+            selectedCard.element.firstChild.id = "";
+            selectedCard.element = null;
+        }  
+    });
+    object.appendChild(newCard);
 }
 
 function checkIfCardAllowed(topCardE){
@@ -59,6 +55,22 @@ function checkIfCardAllowed(topCardE){
         selectedCard.element.remove();
         selectedCard = {};
     }
+}
+
+function drawCard(){
+    if(drawnCards.length >= deckLength){
+        alert("No more cards to draw");
+        return null;
+    }
+
+    let card;
+    do {
+        
+        card = randomCard();
+    } while(drawnCards.includes(card))
+    
+    drawnCards.push(card);
+    return card;
 }
 
 
@@ -96,23 +108,28 @@ function main(){
         // ====== DOM Elements ======
     let topCardE = document.querySelector("#card-throw");
     let handCardsE = document.querySelector("#player-cards");
-    let backSideCard = document.querySelector("#card-bunk");
+    let bunk = document.querySelector("#card-bunk");
 
         // ==== GAME VARIABLES ======
     let botAmount = document.querySelector("#enemyCount").value;
     console.log(botAmount)
 
-    // Objects
-    makePlayerCards(handCardsE)
-    
-
     // ====== Event Listeners ======
     topCardE.addEventListener("click", function(){
         checkIfCardAllowed(topCardE)
     });
-
+    bunk.addEventListener("click", function(){
+        let newCard = drawCard();
+        giveDrawnCard(newCard, handCardsE)
+    })
 
     // ====== Initial Setup ======
     topCardE.innerHTML += `<img src="Assets/Kortbilder/${cardOnTop}.png" alt=""></img>`;
+
+    for(let i = 0; i < starterCardCount; i++){
+        let newCard = drawCard();
+        giveDrawnCard(newCard, handCardsE)
+    }
+
 }
 document.querySelector("#startBtn").addEventListener("click", main);
